81d4b8b47daeb334ae5e4dae5016768e347cdb5a
Pranta-Das <prantodas423@gmail.com>
2025-04-16 18:47:51 +0600

    string length check

---
+++ functionCondition.js
@@ -0,0 +1,8 @@
+function evenSizedString(str){
+    const size = str.length;
+    return size;
+    // console.log(str , size);
+}
+const size = evenSizedString('pranto');
+// evenSizedString('joy');
+console.log(size);
