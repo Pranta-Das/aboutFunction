40d56ff8efe208a4c3603a13a8896f0b779af944
Pranta-Das <prantodas423@gmail.com>
2025-04-16 18:56:34 +0600

    conditional function

--- functionCondition.js
+++ functionCondition.js
@@ -1,3 +1,4 @@
+const prompt = require('prompt-sync')({sigint:true});
 function evenSizedString(str){
     const size = str.length;
     return size;
@@ -6,3 +7,14 @@ function evenSizedString(str){
 const size = evenSizedString('pranto');
 // evenSizedString('joy');
 console.log(size);
+
+function stringSized(str){
+    const size  = str.length;
+    if(size%2 == 0){
+        console.log(size ," is a even number");
+    }else{
+        console.log(size ," is a odd number");
+    }
+}
+const str = prompt("Enter a word or set of character: ");
+stringSized(str);
